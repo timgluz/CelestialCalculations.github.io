bf7fe4de992a676a4cc91ee9a59e918d0f436f0d
Timo Sulg <timo.sulg@creditshelf.com>
2020-01-19 18:00:57 +0100

    start working on datetime conversions;

--- rust/ast_utils/src/unit_test_helpers.rs
+++ rust/ast_utils/src/unit_test_helpers.rs
@@ -3,3 +3,13 @@ pub const DISTANCE_TOLERANCE: f64 = 0.1e-5;
 pub fn is_close(expected_val: f64, real_val: f64) -> bool {
     (expected_val - real_val).abs() <= DISTANCE_TOLERANCE
 }
+
+pub fn assert_close(expected_val: f64, real_val: f64) {
+    assert!(
+        is_close(expected_val, real_val),
+        "value {} differs from {} more than {}",
+        real_val,
+        expected_val,
+        DISTANCE_TOLERANCE
+    )
+}
